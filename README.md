2a36d29a7ba3b47663b9d449785075be8d9e0c5a
Ruoshi Li <ruoshi@amazon.com>
2018-08-17 21:18:29 -0700

    Move how to run this test from Main.java to README.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-This is a set of code for demonstrating the Escape Analysis feature of JVM. 
+This is a set of code for demonstrating the Escape Analysis feature of JVM.
+
+To run this test,
+1. clone this repo
+1. ./gradlew clean && ./gradlew jar
+1. cd build/libs
+1. java -server -XX:+PrintGC -XX:+DoEscapeAnalysis -jar ./build/libs/escape-analysis-demo-1.0-SNAPSHOT.jar ***x***
+1. java -server -XX:+PrintGC -XX:-DoEscapeAnalysis -jar ./build/libs/escape-analysis-demo-1.0-SNAPSHOT.jar ***x***
+ 
+x can be 1 ~ 8 
--- src/main/java/bof/Main.java
+++ src/main/java/bof/Main.java
@@ -1,17 +1,6 @@
 package bof;
 
 public class Main {
-
-    /**
-     * To run this test,
-     * 1. ./gradlew clean && ./gradlew jar
-     * 2. cd build/libs
-     * 3. java -server -XX:+PrintGC -XX:+DoEscapeAnalysis -jar ./build/libs/escape-analysis-demo-1.0-SNAPSHOT.jar x
-     * 4. java -server -XX:+PrintGC -XX:-DoEscapeAnalysis -jar ./build/libs/escape-analysis-demo-1.0-SNAPSHOT.jar x
-     *
-     * x: 1 ~ 8
-     */
-
     public static void main(String[] args)
             throws ClassNotFoundException, IllegalAccessException, InstantiationException, InterruptedException {
         final Runnable demo = (Runnable) Class.forName("bof.Demo" + args[0]).newInstance();
